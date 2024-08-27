8ecbe58a83e566d1fac0483533c4f4779c1e0f6c
Saud Ahmed Kadapa <165421567+saudahmedkadapa@users.noreply.github.com>
2024-08-27 20:31:21 +0530

    Update automation.java

--- Automation/src/main/java/automation.java
+++ Automation/src/main/java/automation.java
@@ -14,7 +14,7 @@ public class automation {
 		WebDriver drive = new ChromeDriver();
 		drive.get("https://www.saucedemo.com");
 		drive.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("Saud Ahmed");
-		drive.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("7447287866");
+		drive.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123456789");
 		
 		//drive.switchTo().newWindow(WindowType.TAB);
 		WebElement lb=drive.findElement(By.xpath("//*[@id=\"login-button\"]"));
